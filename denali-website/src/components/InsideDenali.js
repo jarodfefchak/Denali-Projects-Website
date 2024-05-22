2a6ad88f893ee36ef67eb5744a75691e47e0a316
Alison Gartner <alisongartnermg@gmail.com>
2024-05-22 11:19:33 -0600

    Inside Denali Genral Layout

--- denali-website/src/components/InsideDenali.js
+++ denali-website/src/components/InsideDenali.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { useState } from 'react';
+import InsideImage from "../images/InsideDenali.jpg";
+import {Link} from 'react-router-dom';
 
 function InsideDenali(){
+
+    const [hover,setHoverID] = useState(false);
+    const button = {
+    color: "white",
+    backgroundColor:"#374f59",
+    border: "0px",
+    width:"400px",
+    height:"90px",
+    fontSize: "25pt",
+    cursor: "pointer",
+    ...(hover?{
+      boxShadow: "1px 1px 20px #000000",
+    }:null),
+  };
+
     return(
         <div style = {background}>
-            <div style = {text}>
-            <h2>Inside Denali</h2>
-            </div>  
+            <div style = {content}>
+            <img src = {InsideImage} alt = "inside denali" style = {img} />
+                    <div style = {text}>
+                    <p style = {{fontSize: "60pt"}}><b>Inside Denali </b></p>
+                    <p style = {{fontSize: "20pt"}}>Founded in 2016, Denali Projects has been dedicated to delivering top-tier project management and engineering solutions in the energy industry. 
+                        We prioritize client satisfaction by fostering strong partnerships and delivering projects that meet the highest standards of quality and safety. 
+                        Our mission is to turn complex challenges into successful outcomes, making us a trusted partner in the EPC industry.</p>
+                    <Link to = "/CareerOpportunities" >
+                        <button onMouseEnter={() => setHoverID(true)}
+                        onMouseLeave={() => setHoverID(false)} style = {button}>Discover Who We Are</button>
+                    </Link>
+                </div>
+                </div>  
         </div>
     );
 };
@@ -19,7 +47,18 @@ const background = {
     margin: "0px",
     padding: "0px",
 };
-const text ={
+const content ={
     display: 'flex',
-    flexDirection: 'column',
+    justifyContent:"center",
+    alignItems: "center",
+    padding: "100px",
+
+};
+const img = {
+    width: "50%",
+    height: "80%",
 }
+const text = {
+    padding:"100px",
+    color: "white"
+};
