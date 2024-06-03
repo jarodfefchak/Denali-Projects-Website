b9c552cb9c91a4e37bf364d95e83472b01dbe1af
Alison Gartner <alisongartnermg@gmail.com>
2024-06-03 11:13:22 -0600

    Results Responsive Layout

--- denali-website/src/components/Results.js
+++ denali-website/src/components/Results.js
@@ -1,81 +1,80 @@
 import React, { useState } from 'react';
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
+import './Results.css';
 
 function Results() {
     const [hasBeenVisible, setHasBeenVisible] = useState({ first: false, second: false, third: false });
 
     return (
         <div style={background}>
-            <p style = {{fontSize:"60pt",marginLeft:"100px", marginTop:"50px",}}><b>Our Results</b></p>
-            <div style={stats}>
-                <div style = {counter}>
-                <div style = {count}>
-                <CountUp end={hasBeenVisible.first ? 2586 : 0} duration={6} >
-                    {({ countUpRef, start }) => (
-                        <VisibilitySensor
-                            onChange={(isVisible) => {
-                                if (isVisible && !hasBeenVisible.first) {
-                                    setTimeout(() => {
-                                        start();
-                                        setHasBeenVisible((prev) => ({ ...prev, first: true }));
-                                    }, 250);
-                                }
-                            }}
-                            delayedCall
-                        >
-                            <span ref={countUpRef} />
-                        </VisibilitySensor>
-                    )}
-                   
-                </CountUp>
+            <p className="titleR"><b>Our Results</b></p>
+            <div className="statsR">
+                <div className="counterR">
+                    <div className="countR">
+                        <CountUp end={hasBeenVisible.first ? 2586 : 0} duration={6}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor
+                                    onChange={(isVisible) => {
+                                        if (isVisible && !hasBeenVisible.first) {
+                                            setTimeout(() => {
+                                                start();
+                                                setHasBeenVisible((prev) => ({ ...prev, first: true }));
+                                            }, 250);
+                                        }
+                                    }}
+                                    delayedCall
+                                >
+                                    <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}
+                        </CountUp>
+                    </div>
+                    <p className="descriptionR">Projects to Date</p>
                 </div>
-                <p style ={{fontSize:"45pt"}}>Projects to Date</p>
+                <div className="counterR">
+                    <div className="countR">
+                        <CountUp end={hasBeenVisible.second ? 53 : 0} duration={4}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor
+                                    onChange={(isVisible) => {
+                                        if (isVisible && !hasBeenVisible.second) {
+                                            setTimeout(() => {
+                                                start();
+                                                setHasBeenVisible((prev) => ({ ...prev, second: true }));
+                                            }, 250);
+                                        }
+                                    }}
+                                    delayedCall
+                                >
+                                    <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}
+                        </CountUp>
+                    </div>
+                    <p className="descriptionR">Successful Partnerships</p>
                 </div>
-                <div style = {counter}>
-                <div style = {count}>
-                <CountUp end={hasBeenVisible.second ? 53 : 0} duration={4} >
-                    {({ countUpRef, start }) => (
-                        <VisibilitySensor
-                            onChange={(isVisible) => {
-                                if (isVisible && !hasBeenVisible.second) {
-                                    setTimeout(() => {
-                                        start();
-                                        setHasBeenVisible((prev) => ({ ...prev, second: true }));
-                                    },250);
-                                }
-                            }}
-                            delayedCall
-                        >
-                            <span ref={countUpRef} />
-                        </VisibilitySensor>
-                    )}
-                     
-                </CountUp>
-                </div>
-                <p style ={{fontSize:"45pt"}}>Successful Partnerships</p>
-                </div>
-                <div style = {counter}>
-                    <div style = {count}>
-                    <CountUp end={hasBeenVisible.third ? 96 : 0} duration={6} formattingFn={(value) => `${value}%`}>
-                    {({ countUpRef, start }) => (
-                        <VisibilitySensor
-                            onChange={(isVisible) => {
-                                if (isVisible && !hasBeenVisible.third) {
-                                    setTimeout(() => {
-                                        start();
-                                        setHasBeenVisible((prev) => ({ ...prev, third: true }));
-                                    }, 250);
-                                }
-                            }}
-                            delayedCall
-                        >
-                            <span ref={countUpRef} />
-                        </VisibilitySensor>
-                    )}
-                </CountUp>
-                </div>
-                <p style ={{fontSize:"45pt"}}>Retention Rate</p>
+                <div className="counterR">
+                    <div className="countR">
+                        <CountUp end={hasBeenVisible.third ? 96 : 0} duration={6} formattingFn={(value) => `${value}%`}>
+                            {({ countUpRef, start }) => (
+                                <VisibilitySensor
+                                    onChange={(isVisible) => {
+                                        if (isVisible && !hasBeenVisible.third) {
+                                            setTimeout(() => {
+                                                start();
+                                                setHasBeenVisible((prev) => ({ ...prev, third: true }));
+                                            }, 250);
+                                        }
+                                    }}
+                                    delayedCall
+                                >
+                                    <span ref={countUpRef} />
+                                </VisibilitySensor>
+                            )}
+                        </CountUp>
+                    </div>
+                    <p className="descriptionR">Retention Rate</p>
                 </div>
             </div>
         </div>
@@ -88,25 +87,5 @@ const background = {
     display: 'flex',
     flexDirection: 'column',
     backgroundColor: '#6a696b',
-    color:"white",
-};
-
-const stats = {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginLeft:"100px",
-    marginRight:"100px",
-    marginTop:"50px",
-    marginBottom:"100px",
-};
-
-const counter ={
-    flexDirection:"column",
-    textAlign:"center",
-};
-const count ={
-    fontSize:"80pt",
-    fontWeight:"600"
+    color: "white",
 };
