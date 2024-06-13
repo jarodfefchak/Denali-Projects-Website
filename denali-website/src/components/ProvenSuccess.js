8ff9ab9dafdfc8d5d25888bb11d723913f66c1ed
Alison Gartner <alisongartnermg@gmail.com>
2024-06-13 09:17:57 -0600

    correct carousel transitions

--- denali-website/src/components/ProvenSuccess.js
+++ denali-website/src/components/ProvenSuccess.js
@@ -1,5 +1,4 @@
-
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import Project1Img from "../images/ProvenSuccess/Project1.jpg";
@@ -8,77 +7,60 @@ import Project3Img from "../images/ProvenSuccess/Project3.jpg";
 import "./ProvenSuccess.css";
 
 function ProvenSuccess() {
-  useEffect(() => {
-    window.$("#carouselExampleIndicators").carousel();
-  }, []);
-  
+  const [activeIndex, setActiveIndex] = useState(0);
+
   const handleDragStart = (e) => e.preventDefault();
-  
+
   const items = [
     <img
       src={Project1Img}
       alt="Project 1"
       onDragStart={handleDragStart}
-      data-value="1"
       className="carousel-image"
+      key="Pimg1"
     />,
     <img
       src={Project2Img}
       alt="Project 2"
       onDragStart={handleDragStart}
-      data-value="2"
       className="carousel-image"
+      key="Pimg2"
     />,
     <img
       src={Project3Img}
       alt="Project 3"
       onDragStart={handleDragStart}
-      data-value="3"
       className="carousel-image"
+      key="Pimg3"
     />,
   ];
 
-  const renderDotsItem = ({ isActive }) => {
-    return (
-      <div className={`dot ${isActive ? "active-dot" : "inactive-dot"}`} style={{cursor:"pointer"}}></div>
-    );
-  };
+  const handleSlideChanged = ({ item }) => setActiveIndex(item);
 
-  const renderPrevButton = ({ isDisabled }) => {
-    return (
-      <span
-        className="carousel-arrow carousel-arrow-prev"
-        style={{ opacity: isDisabled ? "0.5" : 1 }}
-      >
-        &lang;
-      </span>
-    );
-  };
-
-  const renderNextButton = ({ isDisabled }) => {
-    return (
-      <span
-        className="carousel-arrow carousel-arrow-next"
-        style={{ opacity: isDisabled ? "0.5" : 1 }}
-      >
-        &rang;
-      </span>
-    );
-  };
-
-  const Carousel = () => (
-    <div className="carousel-container">
-      <AliceCarousel
-        mouseTracking
-        renderPrevButton={renderPrevButton}
-        items={items}
-        renderNextButton={renderNextButton}
-        renderDotsItem={renderDotsItem}
-        infinite
-      />
-    </div>
+  const renderDotsItem = ({ isActive, itemIndex }) => (
+    <div
+      className={`dot ${isActive ? "active-dot" : "inactive-dot"}`}
+      onClick={() => setActiveIndex(itemIndex)}
+    ></div>
   );
-  
+
+  const renderPrevButton = ({ isDisabled }) => (
+    <span
+      className="carousel-arrow carousel-arrow-prev"
+      style={{ opacity: isDisabled ? "0.5" : 1 }}
+    >
+      &lang;
+    </span>
+  );
+
+  const renderNextButton = ({ isDisabled }) => (
+    <span
+      className="carousel-arrow carousel-arrow-next"
+      style={{ opacity: isDisabled ? "0.5" : 1 }}
+    >
+      &rang;
+    </span>
+  );
 
   return (
     <div className="backgroundPS">
@@ -87,10 +69,23 @@ function ProvenSuccess() {
           <b>Proven Success</b>
         </p>
         <p className="subtitlePS">Engineering Energy, Empowering Progress</p>
-        <Carousel />
+        <div className="carousel-container">
+          <AliceCarousel
+            items={items}
+            activeIndex={activeIndex}
+            onSlideChanged={handleSlideChanged}
+            renderPrevButton={renderPrevButton}
+            renderNextButton={renderNextButton}
+            renderDotsItem={renderDotsItem}
+            mouseTracking
+            infinite
+            animationType="fadeout"
+          />
+        </div>
       </div>
     </div>
   );
 }
 
 export default ProvenSuccess;
+
