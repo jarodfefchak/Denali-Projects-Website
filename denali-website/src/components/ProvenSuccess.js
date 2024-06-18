718a95450ef8c8655721671ed79f752e55bc95ae
Alison Gartner <alisongartnermg@gmail.com>
2024-06-18 14:59:30 -0600

    carousel text

--- denali-website/src/components/ProvenSuccess.js
+++ denali-website/src/components/ProvenSuccess.js
@@ -8,100 +8,97 @@ import "./ProvenSuccess.css";
 
 function ProvenSuccess() {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [showInfo, setShowInfo] = useState(false); // State to track if info should be shown
-
-  const handleDragStart = (e) => e.preventDefault();
+  const [showInfo, setShowInfo] = useState(false);
   const [hover, setHover] = useState(false);
 
   const projectInfo = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   ];
+  const handleDragStart = (e) => e.preventDefault();
 
   const items = [
-    <div>
-      <div className="image-container">
+    <div key="project1">
+      <div className={`image-container ${showInfo && activeIndex === 0 ? "blur" : ""}`}>
         <img
           src={Project1Img}
           alt="Project 1"
           onDragStart={handleDragStart}
           className="carousel-image"
-          key="Pimg1"
         />
         {showInfo && activeIndex === 0 && (
-          <div className="info-overlay">
-            <p className="project-info">{projectInfo[0]}</p>
+          <div className={`overlay-text ${showInfo ? "fade-in" : ""}`}>
+            <p>{projectInfo[0]}</p>
           </div>
         )}
+        <button
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          className={`buttonPS ${hover ? "button-hoverPS" : ""}`}
+          onClick={() => {
+            setActiveIndex(0);
+            setShowInfo(!showInfo);
+          }}
+        >
+          {showInfo && activeIndex === 0 ? 'X' : "Project 1 Info"}
+        </button>
       </div>
-      <button
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        className={`buttonPS ${hover ? "button-hoverPS" : ""}`}
-        onClick={() => {
-          setActiveIndex(0);
-          setShowInfo(prevShowInfo => !prevShowInfo);
-        }}
-      >
-        Project 1 Info
-      </button>
     </div>,
-    <div>
-      <div className="image-container">
+    <div key="project2">
+      <div className={`image-container ${showInfo && activeIndex === 1 ? "blur" : ""}`}>
         <img
           src={Project2Img}
           alt="Project 2"
           onDragStart={handleDragStart}
           className="carousel-image"
-          key="Pimg2"
         />
         {showInfo && activeIndex === 1 && (
-          <div className="info-overlay">
-            <p className="project-info">{projectInfo[1]}</p>
+          <div className={`overlay-text ${showInfo ? "fade-in" : ""}`}>
+            <p>{projectInfo[1]}</p>
           </div>
         )}
+        <button
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          className={`buttonPS ${hover ? "button-hoverPS" : ""}`}
+          onClick={() => {
+            setActiveIndex(1);
+            setShowInfo(!showInfo);
+          }}
+        >
+          {showInfo && activeIndex === 1 ? 'X' : "Project 2 Info"}
+        </button>
       </div>
-      <button
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        className={`buttonPS ${hover ? "button-hoverPS" : ""}`}
-        onClick={() => {
-          setActiveIndex(1);
-          setShowInfo(prevShowInfo => !prevShowInfo);
-        }}
-      >
-        Project 2 Info
-      </button>
     </div>,
-    <div>
-      <div className="image-container">
+    <div key="project3">
+      <div className={`image-container ${showInfo && activeIndex === 2 ? "blur" : ""}`}>
         <img
           src={Project3Img}
           alt="Project 3"
           onDragStart={handleDragStart}
           className="carousel-image"
-          key="Pimg3"
         />
         {showInfo && activeIndex === 2 && (
-          <div className="info-overlay">
-            <p className="project-info">{projectInfo[2]}</p>
+          <div className={`overlay-text ${showInfo ? "fade-in" : ""}`}>
+            <p>{projectInfo[2]}</p>
           </div>
         )}
+        <button
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          className={`buttonPS ${hover ? "button-hoverPS" : ""}`}
+          onClick={() => {
+            setActiveIndex(2);
+            setShowInfo(!showInfo);
+          }}
+        >
+          {showInfo && activeIndex === 2 ? 'X' : "Project 3 Info"}
+        </button>
       </div>
-      <button
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        className={`buttonPS ${hover ? "button-hoverPS" : ""}`}
-        onClick={() => {
-          setActiveIndex(2);
-          setShowInfo(prevShowInfo => !prevShowInfo);
-        }}
-      >
-        Project 3 Info
-      </button>
     </div>,
   ];
+  
 
   const handleSlideChanged = ({ item }) => {
     setActiveIndex(item);
@@ -126,6 +123,7 @@ function ProvenSuccess() {
       &lang;
     </span>
   );
+
   const renderNextButton = ({ isDisabled }) => (
     <span
       className="carousel-arrow carousel-arrow-next"
@@ -163,5 +161,3 @@ function ProvenSuccess() {
 }
 
 export default ProvenSuccess;
-
-
